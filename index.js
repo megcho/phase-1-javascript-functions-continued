4feb48f57ba962c41e8baccd23f9560dcb68056e
megan cho <meganlisacho@gmail.com>
2021-04-12 16:14:38 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    let result
+    result = `This Saturday, I want to ${activity}!`;
+    return result;
+ }
+
+ const mondayWork = function(activity="go to the office") {
+     let result
+     result = `This Monday, I will ${activity}.`;
+     return result;
+ }
+
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+        return `You are ${style}${adjective}${style}!`
+    }
+}
